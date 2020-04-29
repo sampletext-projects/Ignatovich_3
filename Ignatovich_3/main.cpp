cb7faf24c8dd6db54c9c5e867e1b4ef43aa2d7e0
sampletext32 <sampletext32@bk.ru>
2020-04-29 23:20:04 +0300

    Add project

---
+++ Ignatovich_3/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "russian");
+	int n;
+	cin >> n;
+
+	double r = 0;
+	double k = 1;
+	int i = 1;
+	int m = 0;
+
+	while (m < n)
+	{
+		r += i / k;
+		k *= 2;
+		i *= -1;
+		m++;
+	}
+	cout << r << "\n";
+
+	system("pause");
+}
